f3405d79f25da34f9590ad8a9d23e892520131d0
Jessica Macaspac <jeccamac@gmail.com>
2022-02-09 20:23:31 -0600

    Powerup Speed add functions
    Add timers and function to enable powerup: speed and boosters; enable/disable powerup visuals

--- Assets/Scripts/PowerupSpeed.cs
+++ Assets/Scripts/PowerupSpeed.cs
@@ -29,4 +29,60 @@ public class PowerupSpeed : MonoBehaviour
             StartCoroutine(PowerUpSequence(playerShip));
         }
     }
+
+    IEnumerator PowerupSequence(PlayerShip playerShip)
+    {
+        // set boolean for detecting lockout
+        _poweredUp = true;
+
+        ActivatePowerup(playerShip);
+        // simulate this object being disabled. We dont REALLY want to disable it bc we still need script behavior to continue functioning
+        DisableObject();
+
+        // wait for the required duration
+        yield return new WaitForSeconds(_powerupDuration);
+        // reset
+        DeactivatePowerup(playerShip);
+        EnableObject();
+
+        // set boolean to release lockout
+        _poweredUp = false;
+    }
+
+    void ActivatePowerup(PlayerShip playerShip)
+    {
+        if(playerShip != null)
+        {
+            // powerup player
+            playerShip.SetSpeed(_speedIncreaseAmount);
+            // visuals
+            playerShip.SetBoosters(true);
+        }
+    }
+
+    void DeactivatePowerup(PlayerShip playerShip)
+    {
+        // revert player powerup - will subtract
+        playerShip?.SetSpeed(-_speedIncreaseAmount);
+        // visuals
+        playerShip?.SetBoosters(false);
+    }
+
+    public void DisableObject()
+    {
+        // disable collider, so it cant be retriggered
+        _colliderToDeactivate.enabled = false;
+        // disable visuals, to simulate deactivated
+        _visualsToDeactivate.SetActive(false);
+        // TODO reactivate particle flash/audio
+    }
+
+    public void EnableObject()
+    {
+        // enable collider, so it can be retriggered
+        _colliderToDeactivate.enabled = true;
+        // enable visuals again, to draw player attention
+        _visualsToDeactivate.SetActive(true);
+        // TODO reactivate particle flash/audio
+    }
 }
